27afc2a2fbc6952e2249eb950615fa46706e5814
PatrickKutch <Patrick.Kutch@gmail.com>
2018-08-20 09:12:08 -0700

    tweaking build

--- dockerBuild/build.sh
+++ dockerBuild/build.sh
@@ -2,4 +2,4 @@
 
 cp ../*.py .
 
-docker build --network=host -t patrickkutch/workloadmapper . 
+docker build --no-cache --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy --network=host -t patrickkutch/workloadmapper .
