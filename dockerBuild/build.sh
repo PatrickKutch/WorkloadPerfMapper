62ae08f145eda280be6f67f6c01b309ecc336d7b
root <root@nd-r2208-53.jf.intel.com>
2018-10-03 10:00:16 +0000

    updates for app deployment

--- dockerBuild/build.sh
+++ dockerBuild/build.sh
@@ -3,3 +3,5 @@
 cp ../*.py .
 
 docker build --no-cache --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$https_proxy --network=host -t patrickkutch/workloadmapper .
+
+docker push patrickkutch/workloadmapper
